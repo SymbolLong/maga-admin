e57fd5f70405c33cd8ba1060fc9b66cd6ce583d9
zhangsl <877857078@qq.com>
2018-05-31 15:00:54 +0800

    管理员项目初始化

---
+++ src/main/java/com/maga/admin/MagaAdminApplication.java
@@ -0,0 +1,12 @@
+package com.maga.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagaAdminApplication.class, args);
+    }
+}
